9a5a732aa8a907329a3ca61852e6f8c3ab711bf0
thisisaaronland <aaron@localhost>
2022-05-05 11:48:42 -0700

    update to enable reading of bz2 files (defaults to off), update vendor deps

--- fs_test.go
+++ fs_test.go
@@ -19,25 +19,35 @@ func TestFSReader(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	source := fmt.Sprintf("fs://%s/fixtures", cwd)
+	source := fmt.Sprintf("fs://%s/fixtures?allow_bz2=1", cwd)
 
 	r, err := NewReader(ctx, source)
 
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	fh, err := r.Read(ctx, "101/736/545/101736545.geojson")
-
-	if err != nil {
-		t.Fatal(err)
+	
+	to_test := []string{
+		"101/736/545/101736545.geojson",
+		"101/736/545/101736545.geojson.bz2",
 	}
 
-	defer fh.Close()
+	for _, path := range to_test {
 
-	_, err = io.Copy(ioutil.Discard, fh)
-
-	if err != nil {
-		t.Fatal(err)
+		fmt.Printf("Read %s\n", path)
+		
+		fh, err := r.Read(ctx, path)
+		
+		if err != nil {
+			t.Fatalf("Failed to read %s, %v", path, err)
+		}
+		
+		defer fh.Close()
+		
+		_, err = io.Copy(ioutil.Discard, fh)
+		
+		if err != nil {
+			t.Fatalf("Failed to copy %s, %v", path, err)
+		}
 	}
 }
